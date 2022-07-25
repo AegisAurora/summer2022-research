632ba3af6e9229c86330b243d0337388800db0df
Dima <58101643+AegisAurora@users.noreply.github.com>
2022-07-25 18:22:18 +0400

    New functions in Main

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from src.ds import example, data
-from src.parse import parse
+from src.utils import parse, parseMulti, removeCause
 from src.runner import runner
 
 
 
 def main():
-    a = parse("./data/Disjunctive-Syllogism")
-    experiments =a.makeAllExps(5)
-    r = runner(path="./test-exp",openAIKey="  ")
-    r.setAI21Key("bearer ")
+    a = parse("./data/conjunctionNA")
+    #a = parseMulti("./data/multi")
+    experiments =a.makeAllExps(30)
+    #experiments =a.makeTrials(10,5)
+    #experiments = a.makeMultiExps(40)
+    r = runner(path="./experiments-7-25/ConNa30",openAIKey="")
+    r.setAI21Key("Bearer ")
     r.add(experiments)
-    r.run(provider="openAI",model="text-davinci-002")
-    
+
+
+    r.run(provider="AI21",model="j1-jumbo")
+    #r.rumMulti()
+    #r.runModels()
+    #r.runTrials()
+
+
+
+
+    #removeCause("./data/test")
 
 
 
